47b608e9f1557d12b46f59d79e50e88530d63175
Asil <90615063+Asroyx@users.noreply.github.com>
2022-10-27 14:08:36 +0300

    first commit

---
+++ outerjoin.sql
@@ -0,0 +1,28 @@
+USE Bahçeişleri
+GO
+SELECT COUNT(*) FROM Müşteriler --142
+SELECT COUNT(*) FROM Siparişler --87
+-- Hangi müşteri hangi siparişi vermiştir ?
+SELECT COUNT(*) FROM Siparişler INNER JOIN Müşteriler
+ON Siparişler.MüşteriNO = Müşteriler.MüşteriNo --10
+
+/*
+1. INNER JOIN : İç birleştirme : Birleştirme Koşulunu sağlayan kayıtları listeler.
+2. OUTER JOIN : Dış Birleştirme : İç birleştirmye ek olarak birleştirme koşulunu
+ sağlamayan kayıtları da listelemek için kullanılır :
+ a) LEFT OUTER JOIN : Sol Dış Birleştirme : 
+  Birleştirme koşulunu sağlayan kayıtlara ek olarak,
+  soldaki tablonun birleştirme koşulunu sağlaMAYAN
+  kayıtlarını da listeler
+  SELECT * FROM Soltablo LEFT OUTER JOIN SağTablo ON Birleştirme Koşulu 
+  b) RIGHT OUTER JOIN : Sağ Dış Birleştirme : 
+  Birleştirme koşulunu sağlayan kayıtlara ek olarak,
+  sağdaki tablonun birleştirme koşulunu sağlaMAYAN
+  kayıtlarını da listeler
+  SELECT * FROM Sağtablo RIGHT OUTER JOIN SolTablo ON Birleştirme Koşulu 
+  c) FULL OUTER JOIN : Tam Dış Birleştirme : 
+  Birleştirme koşulunu sağlayan kayıtlara ek olarak,
+  hem soldaki tablonun hem sağdaki tablonun
+   birleştirme koşulunu sağlaMAYAN kayıtlarını da listeler
+  SELECT * FROM Sağtablo RIGHT OUTER JOIN SolTablo ON Birleştirme Koşulu 
+  */
